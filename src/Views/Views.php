bdafaaaae50101b20e9129d39c3f57340adb8760
DmitriyNovok <d.novokreshennyh@key-g.com>
2022-08-17 00:25:16 +0300

    Class Views

---
+++ src/Views/Views.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Views
+{
+    public static function view()
+    {
+        $request = new Request();
+    }
+}
